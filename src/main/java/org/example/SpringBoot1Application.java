3d61fa72f4674984126305fcb3ec4cbfce94960f
JohnE03 <2101519@eng.asu.edu.eg>
2026-05-04 19:18:46 +0300

    first api

--- src/main/java/org/example/SpringBoot1Application.java
+++ src/main/java/org/example/SpringBoot1Application.java
@@ -2,12 +2,20 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBoot1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot1Application.class, args);
+    }
+
+    @GetMapping
+    public String test() {
+        return "Hello World from Spring Boot!";
     }
 
 }
