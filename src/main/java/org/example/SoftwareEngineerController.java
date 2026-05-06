5eb6df19e779701a8192f0fe83967ac5c3319d6c
JohnE03 <2101519@eng.asu.edu.eg>
2026-05-06 15:30:54 +0300

    implement CRUD operations for SoftwareEngineer entity and update API routes

--- src/main/java/org/example/SoftwareEngineerController.java
+++ src/main/java/org/example/SoftwareEngineerController.java
@@ -1,13 +1,11 @@
 package org.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/software-engineers")
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
     private final SoftwareEngineerService softwareEngineerService;
@@ -20,5 +18,28 @@ public class SoftwareEngineerController {
     @RequestMapping("list")
     public List<SoftwareEngineer> getSoftwareEngineers() {
         return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @PostMapping
+    @RequestMapping("new-eng")
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @DeleteMapping
+    @RequestMapping("delete/{id}")
+    public void deleteSoftwareEngineerById(@PathVariable Integer id){
+        softwareEngineerService.deleteSoftwareEngineerById(id);
+    }
+
+    @PutMapping("{id}")
+    public void updateSoftwareEngineer(@PathVariable Integer id, @RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.updateSoftwareEngineerById(id, softwareEngineer);
     }
 }
--- src/main/java/org/example/SoftwareEngineerService.java
+++ src/main/java/org/example/SoftwareEngineerService.java
@@ -18,4 +18,28 @@ public class SoftwareEngineerService {
         return this.softwareEngineerRepository.findAll(); //returns all fields, not preferable in real implementation
         //use .stream().map()
     }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        this.softwareEngineerRepository.save(softwareEngineer);
+    }
+
+    public SoftwareEngineer getSoftwareEngineerById(Integer id) {
+        return this.softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException(
+                id + "Not found"));
+    }
+
+    public void deleteSoftwareEngineerById(Integer id) {
+        if (!this.softwareEngineerRepository.existsById(id)) {
+            throw new IllegalStateException(id + " not found");
+        }
+        this.softwareEngineerRepository.deleteById(id);
+    }
+
+    public void updateSoftwareEngineerById(Integer id, SoftwareEngineer update) {
+        SoftwareEngineer softwareEng = softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException(
+                id + " not found"));
+        softwareEng.setName(update.getName());
+        softwareEng.setTechStack(update.getTechStack());
+        softwareEngineerRepository.save(softwareEng);
+    }
 }
